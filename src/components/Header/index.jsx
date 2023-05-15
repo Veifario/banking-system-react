8c344f59c9065c9ec9dc38bc731bbcb660f40ae3
Veifario <veifario.git@gmail.com>
2023-05-15 21:38:15 +0500

    some

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/system";
+import React from "react";
+import { useNavigate } from "react-router";
+
+const Header = () => {
+	const navigate = useNavigate();
+
+	return <Box>
+        
+    </Box>;
+};
+
+export default Header;
