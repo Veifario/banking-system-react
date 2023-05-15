0d8a9c8f923fd784ce57094f540ddbd001d5b8d8
Veifario <veifario.git@gmail.com>
2023-05-15 21:23:04 +0500

    register logic

--- src/api/postRequest.js
+++ src/api/postRequest.js
@@ -4,3 +4,8 @@ export const loginUser = async (user) => {
 	const { data } = await instance.post("/login", user);
 	return data;
 };
+
+export const registerUser = async (user) => {
+	const { data } = await instance.post("/register", user);
+	return data;
+};
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,5 +1,6 @@
 import {
 	Box,
+	Button,
 	FormControl,
 	Grid,
 	InputLabel,
@@ -9,52 +10,133 @@ import {
 } from "@mui/material";
 import s from "./index.module.scss";
 import React, { useState } from "react";
+import { registerUser } from "../../api/postRequest";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-	const inputs = useState({
+	const navigate = useNavigate();
+	const [inputs, setInputs] = useState({
 		username: "",
 		password: "",
+		name: "",
 		phone_number: "",
 		address: "",
 		email: "",
 		type: "",
 	});
 
-	// {'username': 'username', 'password': 'password', 'name': 'name', 'address': 'address',
-	// # 'phone_number': 'phone_number', 'email': 'email', 'user_type': 'user_type'}
+	const signup = async () => {
+		for (const input in inputs) {
+			if (inputs[input].trim() === "") {
+
+				return;
+			}
+		}
+
+		const newUser = new Object({
+			username: inputs.username,
+			password: inputs.password,
+			name: inputs.name,
+			address: inputs.address,
+			phone_number: inputs.phone_number,
+			email: inputs.email,
+			user_type: inputs.type,
+		});
+
+		
+		await registerUser(newUser);
+		navigate("/login");
+	};
 
 	return (
 		<Box className={s.root}>
 			<FormControl>
 				<Grid container spacing={3}>
 					<Grid item xs={4}>
-						<TextField label="Username" variant="standard" fullWidth />
+						<TextField
+							value={inputs.username}
+							onChange={({ target }) =>
+								setInputs({ ...inputs, username: target.value })
+							}
+							label="Username"
+							variant="standard"
+							fullWidth
+						/>
 					</Grid>
 					<Grid item xs={4}>
-						<TextField label="Password" variant="standard" fullWidth />
+						<TextField
+							value={inputs.password}
+							onChange={({ target }) =>
+								setInputs({ ...inputs, password: target.value })
+							}
+							label="Password"
+							variant="standard"
+							fullWidth
+						/>
 					</Grid>
 					<Grid item xs={4}>
-						<TextField label="Name" variant="standard" fullWidth />
+						<TextField
+							value={inputs.name}
+							onChange={({ target }) =>
+								setInputs({ ...inputs, name: target.value })
+							}
+							label="Name"
+							variant="standard"
+							fullWidth
+						/>
 					</Grid>
 					<Grid item xs={8}>
-						<TextField label="Address" variant="standard" fullWidth />
+						<TextField
+							value={inputs.address}
+							onChange={({ target }) =>
+								setInputs({ ...inputs, address: target.value })
+							}
+							label="Address"
+							variant="standard"
+							fullWidth
+						/>
 					</Grid>
 					<Grid item xs={4}>
-						<TextField label="Phone number" variant="standard" fullWidth />
+						<TextField
+							value={inputs.phone_number}
+							onChange={({ target }) =>
+								setInputs({ ...inputs, phone_number: target.value })
+							}
+							label="Phone number"
+							variant="standard"
+							fullWidth
+						/>
 					</Grid>
 					<Grid item xs={4}>
-						<TextField label="Email" variant="standard" fullWidth />
+						<TextField
+							value={inputs.email}
+							onChange={({ target }) =>
+								setInputs({ ...inputs, email: target.value })
+							}
+							label="Email"
+							variant="standard"
+							fullWidth
+						/>
 					</Grid>
 					<Grid item xs={16}>
 						<FormControl fullWidth>
 							<InputLabel>Type</InputLabel>
-							<Select value={inputs.type} label="Type" onChange={() => {}}>
+							<Select
+								value={inputs.type}
+								label="Type"
+								onChange={({ target }) =>
+									setInputs({ ...inputs, type: target.value })
+								}
+							>
 								<MenuItem value="credit">Credit</MenuItem>
 								<MenuItem value="deposit">Deposit</MenuItem>
 							</Select>
 						</FormControl>
 					</Grid>
 				</Grid>
+				<Button type="submit" onClick={signup}>
+					Sign Up
+				</Button>
 			</FormControl>
 		</Box>
 	);
